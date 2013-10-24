231381886a45a3e9f5d81c7470410a1b8aae8d45
Max Jendruk <max.jendruk@student.hpi.uni-potsdam.de>
2013-10-24 11:56:44 +0200

    add ArgumentsParser with scaling options

--- source/glraw-cmd/main.cpp
+++ source/glraw-cmd/main.cpp
@@ -12,105 +12,7 @@
 
 #include <QCoreApplication>
 
-#include <version.h>
-
-
-enum Format
-{
-    GL_RED
-,   GL_RG
-,   GL_RGB
-,   GL_BGR
-,   GL_RGBA
-,   GL_BGRA
-,   GL_STENCIL_INDEX
-,   GL_DEPTH_COMPONENT
-,   GL_DEPTH_STENCIL
-};
-
-enum Type
-{
-    GL_UNSIGNED_BYTE
-,   GL_BYTE
-,   GL_UNSIGNED_SHORT
-,   GL_SHORT
-,   GL_UNSIGNED_INT
-,   GL_INT
-,   GL_FLOAT
-};
-
-
-QMap<QString, Format> g_formats;
-QMap<QString, Type> g_types;
-QVector<QString> g_options;
-QStringList g_manuals;
-QStringList g_versions;
-
-void initialize()
-{
-    g_formats["GL_RED"] = GL_RED;
-    g_formats["GL_RG"] = GL_RG;
-    g_formats["GL_RGB"] = GL_RGB;
-    g_formats["GL_BGR"] = GL_BGR;
-    g_formats["GL_RGBA"] = GL_RGBA;
-    g_formats["GL_BGRA"] = GL_BGRA;
-    //g_formats["GL_STENCIL_INDEX"] = GL_STENCIL_INDEX;
-    //g_formats["GL_DEPTH_COMPONENT"] = GL_DEPTH_COMPONENT;
-    //g_formats["GL_DEPTH_STENCIL"] = GL_DEPTH_STENCIL;
-
-    g_types["GL_UNSIGNED_BYTE"] = GL_UNSIGNED_BYTE;
-    g_types["GL_BYTE"] = GL_BYTE;
-    g_types["GL_UNSIGNED_SHORT"] = GL_UNSIGNED_SHORT;
-    g_types["GL_SHORT"] = GL_SHORT;
-    g_types["GL_UNSIGNED_INT"] = GL_UNSIGNED_INT;
-    g_types["GL_INT"] = GL_INT;
-    g_types["GL_FLOAT"] = GL_FLOAT;
-
-    g_options << "--mirror-horizontal"  << "-mh" ;
-    g_options << "--mirror-vertical"    << "-mv" ;
-    g_options << "--as-bgr"             << "-bgr";
-    g_options << "--quiet"              << "-q";
-
-    g_manuals  << "--help" << "-h" << "/?";
-    g_versions << "--version" << "-v";
-}
-
-static const int option_mh  = 0;
-static const int option_mv  = 2;
-static const int option_bgr = 4;
-static const int option_q   = 6;
-
-void man()
-{
-    qDebug() << "";
-    qDebug() << GLRAW_PROJECT_NAME << GLRAW_VERSION << "by" << GLRAW_AUTHOR_ORGANIZATION;
-    qDebug() << "Converts an input image to an OpenGL compatible format.";
-    qDebug() << "";
-    qDebug() << "usage: glraw <Target-Format> <Target-Type> [<Options>] <Input-FilePath>";
-    qDebug() << "";
-    qDebug() << "<Target-Formats>:";
-    qDebug().nospace() << "  " << qPrintable(QStringList(g_formats.keys()).join("\n  "));
-    qDebug() << "";
-    qDebug() << "<Target-Types>:";
-    qDebug().nospace() << "  " << qPrintable(QStringList(g_types.keys()).join("\n  "));
-    qDebug() << "";
-    qDebug() << "<Options>:";
-    qDebug().nospace() << "  " << qPrintable(g_options[option_mh])  << " (" << qPrintable(g_options[option_mh + 1])  << ")";
-    qDebug().nospace() << "  " << qPrintable(g_options[option_mv])  << " (" << qPrintable(g_options[option_mv + 1])  << ")";
-    qDebug().nospace() << "  " << qPrintable(g_options[option_bgr]) << " (" << qPrintable(g_options[option_bgr + 1]) << ")";
-    qDebug().nospace() << "  " << qPrintable(g_options[option_q])   << " (" << qPrintable(g_options[option_q + 1])   << ")";
-    qDebug() << "";
-}
-
-void version()
-{
-    qDebug() << GLRAW_PROJECT_NAME << GLRAW_VERSION;
-    qDebug() << "";
-}
-
-void quietMessageHandler(QtMsgType, const QMessageLogContext &, const QString &)
-{
-}
+#include "ArgumentsParser.h"
 
 template<typename T>
 void write(
@@ -150,67 +52,18 @@ int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
-    initialize();
+    ArgumentsParser parser(a.arguments());
 
-    if (a.arguments().size() == 1 || g_manuals.contains(a.arguments()[1]))
-    {
-        man();
+    if (!parser.isValid())
         return 0;
-    }
 
-    if (g_versions.contains(a.arguments()[1]))
-    {
-        version();
-        return 0;
-    }
-
-    if (a.arguments().contains(g_options[option_q]) || a.arguments().contains(g_options[option_q + 1]))
-        qInstallMessageHandler(quietMessageHandler);
-
-    const QString farg = a.arguments()[1];
-    if(!g_formats.contains(farg))
-    {
-        qWarning() << "Unknown Target-Format" << qPrintable(farg) << ".";
-        return 0;
-    }
-    const Format format = g_formats[farg];
-
-
-    const QString targ = a.arguments()[2];
-    if(!g_types.contains(targ))
-    {
-        qWarning() << "Unknown Target-Type" << qPrintable(targ) << ".";
-        return 0;
-    }
-    const Type type = g_types[targ];
-
-    const QStringList args(a.arguments());
-
-    const bool mh  = args.contains(g_options[option_mh])  || args.contains(g_options[option_mh + 1]);
-    const bool mv  = args.contains(g_options[option_mv])  || args.contains(g_options[option_mv + 1]);
-    const bool bgr = args.contains(g_options[option_bgr]) || args.contains(g_options[option_bgr + 1]);
-
-    // catch unknown args
-
-    QStringList unknowns;
-    for (int i = 1; i < args.size() - 1; ++i)
-    {
-        if (!g_options.contains(args[i]) && !g_formats.contains(args[i]) && !g_types.contains(args[i]))
-            unknowns << args[i];
-    }
-    if (!unknowns.isEmpty())
-        qWarning() << "The following unknown arguments will be ignored:" << unknowns.join(", ");
-
-    const QString iarg = a.arguments().last();
-
-    if(!QFile::exists(iarg))
+    if(!QFile::exists(parser.filePath()))
     {
         qDebug() << "Input file does not exist.";
         return 0;
     }
 
-
-    QImage image(iarg);
+    QImage image(parser.filePath());
     if(image.isNull())
     {
         qDebug() << "Loading file from input file failed.";
@@ -239,19 +92,19 @@ int main(int argc, char *argv[])
     const QString suffixes = QString(".%1.%2.%3.%4.raw")
             .arg(image.width())
             .arg(image.height())
-            .arg(formatSuffixes[format])
-            .arg(typeSuffixes[type]);
+            .arg(formatSuffixes[parser.format()])
+            .arg(typeSuffixes[parser.type()]);
 
-    QFileInfo fi(iarg);
+    QFileInfo fi(parser.filePath());
 
     const QString target = fi.absolutePath() + "/" + fi.baseName() + suffixes;
 
     // prepare ?
 
-    if (GL_BGR != format && GL_BGRA != format)
+    if (GL_BGR != parser.format() && GL_BGRA != parser.format())
         image = image.rgbSwapped();
 
-    image = image.mirrored(mh, !mv);
+    image = image.mirrored(parser.mirrorHorizontal(), !parser.mirrorVertical());
 
 
     // write
@@ -265,28 +118,28 @@ int main(int argc, char *argv[])
 
     QDataStream output(&file);
 
-    switch(type)
+    switch(parser.type())
     {
     case GL_UNSIGNED_BYTE:
-        write<unsigned char>(output, image, format);
+        write<unsigned char>(output, image, parser.format());
         break;
     case GL_BYTE:
-        write<char>(output, image, format);
+        write<char>(output, image, parser.format());
         break;
     case GL_UNSIGNED_SHORT:
-        write<unsigned short>(output, image, format);
+        write<unsigned short>(output, image, parser.format());
         break;
     case GL_SHORT:
-        write<short>(output, image, format);
+        write<short>(output, image, parser.format());
         break;
     case GL_UNSIGNED_INT:
-        write<unsigned int>(output, image, format);
+        write<unsigned int>(output, image, parser.format());
         break;
     case GL_INT:
-        write<int>(output, image, format);
+        write<int>(output, image, parser.format());
         break;
     case GL_FLOAT:
-        write<float>(output, image, format);
+        write<float>(output, image, parser.format());
         break;
     default:
         qWarning() << "Should not happen :P.";
