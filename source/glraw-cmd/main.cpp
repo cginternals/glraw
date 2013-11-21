18a7a6c3514b5ed0be2be07067f1356c3e88d481
Max Jendruk <max.jendruk@student.hpi.uni-potsdam.de>
2013-11-21 17:13:10 +0100

    remove old includes

--- source/glraw-cmd/main.cpp
+++ source/glraw-cmd/main.cpp
@@ -1,8 +1,4 @@
 
-#include <cassert>
-#include <type_traits>
-#include <iostream>
-
 #include <QMap>
 #include <QDebug>
 #include <QImage>
@@ -10,28 +6,10 @@
 #include <QDataStream>
 #include <QFileInfo>
 #include <QStringList>
-
 #include <QCoreApplication>
-#include <QCommandLineParser>
-
-#include <glraw/ConvertManager.h>
-#include <glraw/RawConverter.h>
-#include <glraw/GLRawFileWriter.h>
-#include <glraw/MirrorEditor.h>
-#include <glraw/ScaleEditor.h>
-#include <glraw/GLRawFile.h>
-#include <glraw/RawFile.h>
 
 #include "Application.h"
 #include "Builder.h"
-
-void messageHandler(QtMsgType type, const QMessageLogContext & context, const QString & message)
-{
-    if (type == QtDebugMsg)
-        return;
-
-    std::cout << message.toStdString() << std::endl;
-}
 
 int main(int argc, char * argv[])
 {
