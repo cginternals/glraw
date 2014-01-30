699fabefa0563d1784b33032565761640738abe9
Daniel Limberger <daniel.limberger@hpi.uni-potsdam.de>
2014-01-30 14:55:45 +0100

    refine s3tc compression defines

---
+++ include/glraw/S3TCExtensions.h
@@ -0,0 +1,12 @@
+
+#pragma once 
+
+#include <QtGui/qopengl.h>
+
+#if defined(WIN32) || !defined(GL_EXT_texture_compression_s3tc)
+#define GLRAW_DXT
+#elif defined(__APPLE__) || defined(GL_EXT_texture_compression_s3tc)
+#define GLRAW_S3TC
+#elif defined(GL_EXT_texture_compression_s3tc)
+#define GLRAW_DXT
+#endif
