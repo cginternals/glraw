ecd3e560acb1d7a2b28a64e56cdc310cd1e199a3
Max Jendruk <max.jendruk@student.hpi.uni-potsdam.de>
2014-01-30 15:47:23 +0100

    fix S3TC extensions check

--- include/glraw/S3TCExtensions.h
+++ include/glraw/S3TCExtensions.h
@@ -3,10 +3,10 @@
 
 #include <QtGui/qopengl.h>
 
-#if defined(WIN32) || !defined(GL_EXT_texture_compression_s3tc)
+#if defined(WIN32) && !defined(GL_EXT_texture_compression_s3tc)
 #define GLRAW_DXT
-#elif defined(__APPLE__) || defined(GL_EXT_texture_compression_s3tc)
-#define GLRAW_S3TC
+#elif defined(__APPLE__) && defined(GL_EXT_texture_compression_s3tc)
+#define GLRAW_DXT
 #elif defined(GL_EXT_texture_compression_s3tc)
 #define GLRAW_DXT
 #endif
